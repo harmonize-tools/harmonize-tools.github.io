515af4b56551a049b35d73ec07792023717519aa
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-02-22 11:22:15 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/next-gitlab-pages/' : '',
+  assetPrefix: isProd ? '/harmonize/' : '',
   trailingSlash: true
 }
