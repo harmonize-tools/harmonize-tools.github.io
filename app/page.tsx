9d284d35110788daa3e61973a56bf42523a561f0
ando17 <78936318+ando17@users.noreply.github.com>
2024-04-09 11:59:24 +0200

    Update page.tsx
    added icon for data source list

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const projects = [
     name: 'Data sources list',
     description:
       'Simplified version of the live spreadsheet for the Harmonize website',
-    src: './cards/insects.jpg',
+    src: './cards/data_source_list.svg',
     link: 'data-sources-list',
   },
   {
