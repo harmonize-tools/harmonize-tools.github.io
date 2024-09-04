13453779b12e43f9eca9edac3cc306050f8c9e16
GitHub Action <action@github.com>
2024-09-04 12:05:44 +0000

    Added changes

---
+++ app/(toolkits)/climate-downscaling/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import path from 'path';
+import ProjectLayout from '@/app/project-layout';
+
+const properties_to_check = ['links', 'licenses', 'citations', 'authors', 'badges'];
+const repository  = 'climate-downscaling';
+const config_data = require(`../../../public/repositories/${repository}/harmonize_readme/config.json`);
+const path_html   = path.join('repositories', repository, 'harmonize_readme', 'README.html').replace(/\\/g, '/')
+
+// If the file does not exits create it
+
+
+// If the labels are empty create it empty
+properties_to_check.forEach(property => {
+  if (!config_data.hasOwnProperty(property) || !config_data[property] || !Array.isArray(config_data[property])) {
+    config_data[property] = [];
+  }
+});
+
+export default function Page({
+  params,
+}: {
+  params: { name: string; description: string };
+}) {
+  return (
+    <ProjectLayout
+      links     = {config_data.links}
+      licenses  = {config_data.licenses}
+      citations = {config_data.citations}
+      authors   = {config_data.authors}
+      badges    = {config_data.badges}
+    >
+      <iframe src={path_html} width="100%" height="1000"></iframe>
+    </ProjectLayout>
+  );
+}
